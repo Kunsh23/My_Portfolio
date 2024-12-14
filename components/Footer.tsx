3ff8d71b393b384a4bf7736b82fce268016c9cea
Kunsh Suneja <154256562+Kunsh23@users.noreply.github.com>
2024-12-14 12:36:59 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
           Copyright © 2024 Kunsh Suneja
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6 mt-5">
+        <div className="flex items-center md:gap-3 gap-6 mt-10">
           {socialMedia.map((info) => (
             <div
               key={info.id}
